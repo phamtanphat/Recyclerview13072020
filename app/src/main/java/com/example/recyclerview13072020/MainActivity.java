8ecf1c8dd47123aa4958cf7da2d3c9573e62b667
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-08-14 19:03:22 +0700

    data

--- app/src/main/java/com/example/recyclerview13072020/MainActivity.java
+++ app/src/main/java/com/example/recyclerview13072020/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.recyclerview13072020;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView mRcvFood;
+    FoodAdapter mFoodAdapter;
+    ArrayList<Food> mArrayFoods;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRcvFood = findViewById(R.id.recyclerviewFood);
+        mArrayFoods = new ArrayList<>();
+        mArrayFoods.add(new Food(R.drawable.hinh_mon_banh_khot,"Bánh khọt","Món ăn miền trung",20000));
+        mArrayFoods.add(new Food(R.drawable.hinh_mon_banh_my,"Bánh mỳ","Món ăn nổi tiếng của Việt Nam",25000));
+        mArrayFoods.add(new Food(R.drawable.hinh_mon_bi_ngoi_chien_xu,"Bí ngòi chiên xù","Món ăn lạ hấp dẫn",30000));
+        mArrayFoods.add(new Food(R.drawable.hinh_mon_bun_mam,"Bún mắm","Bún mắm đặc sản quán cô Ba",40000));
+        mArrayFoods.add(new Food(R.drawable.hinh_mon_bun_rieu,"Bún riêu","Bún mắm đặc sản quán cô Ba",25000));
+        mArrayFoods.add(new Food(R.drawable.hinh_mon_muc,"Mực nướng","Hải sản tươi sống",25000));
+
     }
 }
